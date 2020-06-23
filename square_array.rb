08ec84ce6f1177a8a4353fa24b3e73ef85d55abc
MUHANNAD JABER <abujaber44@gmail.com>
2020-06-23 19:34:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
   array = [1, 2, 3]
-  #array.each do |number|
-   # square = number**2 
- #array_sq = [square]   
+  array.each do |number|
+    square = number**2 
+ array_sq = [square]   
 
-array.each do |number, result|
-  result[number] = number**2 
+
 end
 end
