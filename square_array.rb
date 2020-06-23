d109a6c3392d41e988b3d14a4d34e61804c9ae3f
MUHANNAD JABER <abujaber44@gmail.com>
2020-06-23 17:19:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array = [1, 2, 3]
   array.each do |number|
-    puts "#{number}"**2 
+    puts number**2 
     
     #number_square = number**2
     #return number_square
